85f422bdb22f46f51949d2c8f18a6fa16adb7991
MonoKuroEV <ailes.study.ev@gmail.com>
2023-03-03 15:47:52 +0900

    シーダファイル変更

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,35 +17,97 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        
+        // masterアカウント
+        DB::table('users')->insert([
+            'name' => 'master1',
+            'email' => 'master1@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => '2',
+        ]);
+        
+        DB::table('users')->insert([
+            'name' => 'master2',
+            'email' => 'master2@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => '2',
+        ]);
+        
+        DB::table('users')->insert([
+            'name' => 'master3',
+            'email' => 'master3@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => '2',
+        ]);
 
+        DB::table('users')->insert([
+            'name' => 'admin1',
+            'email' => 'admin1@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => '1',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'admin2',
+            'email' => 'admin2@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => '1',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'admin3',
+            'email' => 'admin3@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => '1',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'test1',
+            'email' => 'test1@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => '0',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'test2',
+            'email' => 'test2@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => '0',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'test3',
+            'email' => 'test3@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => '0',
+        ]);
+        
         // カテゴリー
         DB::table('categories')->insert([
             'name' => 'no-category',
         ]);
 
-        // masterアカウント
-        DB::table('users')->insert([
-            'name' => 'master1',
-            'email' => 'master@1',
-            'password' => Hash::make('12345678'),
-            'role' => '2',
+        DB::table('categories')->insert([
+            'name' => '青果',
         ]);
 
-        DB::table('users')->insert([
-            'name' => 'master2',
-            'email' => 'master@2',
-            'password' => Hash::make('12345678'),
-            'role' => '2',
+        DB::table('categories')->insert([
+            'name' => '精肉',
         ]);
 
-        DB::table('users')->insert([
-            'name' => 'master3',
-            'email' => 'master@3',
-            'password' => Hash::make('12345678'),
-            'role' => '2',
+        DB::table('categories')->insert([
+            'name' => '鮮魚',
         ]);
 
+        // 商品
+        DB::table('items')->insert([
+            'user_id' => '0',
+            'name' => 'りんご',
+            'status' => 'active',
+            'category_id' => '1',
+            'detail' => 'りんごです。<br> りんごです。',
+        ]);
 
-
+        
     }
 }
